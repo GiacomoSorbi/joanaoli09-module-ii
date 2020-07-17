599503e485b6271107267f94708c190d90dea9d2
Joana Leitão Oliveira <joanaleitaooliveira@MacBook-Air-de-Joana.local>
2020-07-17 16:18:43 +0100

    Updates

---
+++ precache-manifest.59b22e7a0f0bee363815a885ffccbe3d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9aa7708a8f2a7e5e77298a476dd383",
+    "url": "/module-ii/index.html"
+  },
+  {
+    "revision": "a173fdb8b032052d174d",
+    "url": "/module-ii/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "56f09875deae2e0d62e2",
+    "url": "/module-ii/static/css/main.9086e581.chunk.css"
+  },
+  {
+    "revision": "a173fdb8b032052d174d",
+    "url": "/module-ii/static/js/2.ffcf2f5a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/module-ii/static/js/2.ffcf2f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f09875deae2e0d62e2",
+    "url": "/module-ii/static/js/main.d950364a.chunk.js"
+  },
+  {
+    "revision": "f33a1dd30f730985deaf",
+    "url": "/module-ii/static/js/runtime-main.e13ab09e.js"
+  },
+  {
+    "revision": "3184f13b8b7550923e234f432f101ef2",
+    "url": "/module-ii/static/media/aboutImage.3184f13b.jpg"
+  },
+  {
+    "revision": "d28d31760687330e86d07d5a1ff6ff1f",
+    "url": "/module-ii/static/media/heroImage.d28d3176.jpg"
+  },
+  {
+    "revision": "eb5ab89914f2c88ff18f9b773f5a2c63",
+    "url": "/module-ii/static/media/homeImage1.eb5ab899.jpg"
+  },
+  {
+    "revision": "14a16db6fef82f6fa3084dd69029ace6",
+    "url": "/module-ii/static/media/homeImage2.14a16db6.jpg"
+  },
+  {
+    "revision": "981e53997d57ab91ca7eae3c3372fa21",
+    "url": "/module-ii/static/media/homeImage3.981e5399.jpg"
+  },
+  {
+    "revision": "d155ef0b714f08c5384e93d8a5a0ab66",
+    "url": "/module-ii/static/media/lastrecipe1.d155ef0b.jpg"
+  },
+  {
+    "revision": "45f153fa36bd41a57c39e5da0d8e0c54",
+    "url": "/module-ii/static/media/lastrecipe2.45f153fa.jpg"
+  },
+  {
+    "revision": "56224f65efb76e06e376beb44cef160d",
+    "url": "/module-ii/static/media/lastrecipe3.56224f65.jpg"
+  }
+]);
